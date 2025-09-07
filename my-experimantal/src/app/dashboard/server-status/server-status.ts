6877ad8be4748c6d48ad3858f12792259b0cc605
Aditya Rathore <aditya.rats@gmail.com>
2025-09-07 16:57:53 -0700

    Split component commit

---
+++ my-experimantal/src/app/dashboard/server-status/server-status.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-server-status',
+  imports: [],
+  templateUrl: './server-status.html',
+  styleUrl: './server-status.css'
+})
+export class ServerStatusComponent {
+  currentStatus = 'online';
+
+}
